202f5a77e05b4660d1c2c2e751ac738b49059a23
Sergey Volkov <sv.volkoff@gmail.com>
2018-07-23 00:21:30 +0400

    Section 4. Lesson 29

--- src/app.js
+++ src/app.js
@@ -1,83 +1,67 @@
-function getLocation(location) {
-  if (location) {
-    return <p>Location: {location}</p>;
+class IndecisionApp extends React.Component {
+  render() {
+    const title = 'Indecision';
+    const subtitle = 'Put your life in the hands of a computer';
+    const options = ['Thing one', 'Thing two', 'Thing three'];
+
+    return (
+      <div>
+        <Header title={title} subtitle={subtitle} />
+        <Action />
+        <Options options={options} />
+        <AddOption />
+      </div>
+    );
   }
 }
 
-const app = {
-  title: 'Indecision App',
-  subtitle: 'Put your life in the hands of a computer',
-  options: ['One', 'Two']
-};
-
-const user = {};
-
-let count = 0;
-
-// eslint-disable-next-line
-console.log('App.js is running');
-
-const addOne = () => {
-  count += 1;
-  renderApp();
-};
-
-const minusOne = () => {
-  count -= 1;
-  renderApp();
-};
-
-const reset = () => {
-  count = 0;
-  renderApp();
-};
-
-const onFormSubmit = event => {
-  event.preventDefault();
-
-  const option = event.target.elements.option.value;
-
-  if (option) {
-    app.options.push(option);
-    event.target.elements.option.value = '';
-    renderApp();
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>{this.props.title}</h1>
+        <h2>{this.props.subtitle}</h2>
+      </div>
+    );
   }
-};
+}
 
-const onRemoveAll = () => {
-  app.options = [];
-  renderApp();
-};
+class Action extends React.Component {
+  handlePick() {
+    alert('handlePick');
+  }
 
-const onMakeDecision = () => {
-  const randomNum = Math.floor(Math.random() * app.options.length);
-  const option = app.options[randomNum];
-  alert(option);
-};
+  render() {
+    return (
+      <div>
+        <button onClick={this.handlePick}>What should I do?</button>
+      </div>
+    );
+  }
+}
 
-const appRoot = document.getElementById('app');
+class Options extends React.Component {
+  render() {
+    return (
+      <div>
+        {this.props.options.map(option => (
+          <Option key={option} option={option} />
+        ))}
+      </div>
+    );
+  }
+}
 
-const renderApp = () => {
-  const template = (
-    <div>
-      <h1>{app.title}</h1>
-      {app.subtitle && <p>{app.subtitle}</p>}
-      <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-      <button disabled={app.options.length === 0} onClick={onMakeDecision}>
-        What should I do
-      </button>
-      <button onClick={onRemoveAll}>Remove All</button>
-      <ol>
-        {app.options.map(option => <li key={option}>Option {option}</li>)}
-      </ol>
-      <form onSubmit={onFormSubmit}>
-        <input type="text" name="option" />
-        <button>Add option</button>
-      </form>
-    </div>
-  );
+class Option extends React.Component {
+  render() {
+    return <p>{this.props.option}</p>;
+  }
+}
 
-  ReactDOM.render(template, appRoot);
-};
+class AddOption extends React.Component {
+  render() {
+    return <div>AddOption component here</div>;
+  }
+}
 
-renderApp();
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -13,11 +13,58 @@ class Person {
   }
 }
 
-const me = new Person('Sergey Volkoff', 36);
+class Student extends Person {
+  constructor(name, age, major) {
+    super(name, age);
+    this.major = major;
+  }
 
-console.log(me.getGreating());
+  hasMajor() {
+    return !!this.major;
+  }
+
+  getDescription() {
+    let description = super.getDescription();
+
+    if (this.hasMajor()) {
+      description += ` Their major is ${this.major}.`;
+    }
+    return description;
+  }
+}
+
+class Traveler extends Person {
+  constructor(name, age, homeLocation) {
+    super(name, age);
+    this.homeLocation = homeLocation;
+  }
+
+  hasHomeLocation() {
+    return this.homeLocation;
+  }
+
+  getGreating() {
+    let greating = super.getGreating();
+
+    if (this.hasHomeLocation()) {
+      greating += ` I'm visiting from ${this.homeLocation}`;
+    }
+
+    return greating;
+  }
+}
+
+const me = new Student('Sergey Volkoff', 36, 'Computer Science');
+
+console.log(me);
 console.log(me.getDescription());
+console.log(me.hasMajor());
 
-const other = new Person();
-console.log(other.getGreating());
+const traveler = new Traveler('Shutov', 35, 'Engels');
+console.log(traveler);
+console.log(traveler.getGreating());
+
+const other = new Student();
+console.log(other);
 console.log(other.getDescription());
+console.log(other.hasMajor());
