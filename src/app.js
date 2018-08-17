09e8aec3631fdea7878d3cfd081751e61ddc14aa
sv.volkoff@gmail.com <sv.volkoff@gmail.com>
2018-08-17 13:44:16 +0400

    Section 9. Lecture 80. Organizing our routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));
+const ExpenseDashboardPage = () => (
+  <div>This is from my Dashboard component</div>
+);
+
+const AddExpensePage = () => <div>This is from my add expense component</div>;
+
+const EditExpensePage = () => <div>This is from my edit expense component</div>;
+
+const ExpenseHelpPage = () => <div>This is from my help expense component</div>;
+
+const NotFoundPage = () => (
+  <div>
+    404! - <Link to="/">Go Home</Link>
+  </div>
+);
+
+const Header = () => (
+  <header>
+    <h1>Expensify</h1>
+    <NavLink to="/" activeClassName="is-active" exact>
+      Home
+    </NavLink>
+    <NavLink to="/create" activeClassName="is-active">
+      Create Expense
+    </NavLink>
+    <NavLink to="/edit" activeClassName="is-active">
+      Edit Expense
+    </NavLink>
+    <NavLink to="/help" activeClassName="is-active">
+      Help
+    </NavLink>
+  </header>
+);
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={ExpenseDashboardPage} />
+        <Route path="/create" component={AddExpensePage} />
+        <Route path="/edit" component={EditExpensePage} />
+        <Route path="/help" component={ExpenseHelpPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
