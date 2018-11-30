0c408a97583149aaf2027907f4b6e690340b2a34
sv.volkoff@gmail.com <sv.volkoff@gmail.com>
2018-12-01 01:03:16 +0400

    Section 13. Lecture 131.

--- src/app.js
+++ src/app.js
@@ -3,40 +3,11 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routes/routes';
 import configureStore from './store/configure.store';
-import { addExpense } from './actions/expenses.actions';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
-
-store.dispatch(
-  addExpense({
-    description: 'Water bill',
-    note: 'Some Water bill note',
-    amount: 4500
-  })
-);
-
-store.dispatch(
-  addExpense({
-    description: 'Gas bill',
-    note: 'Some Gas bill note',
-    createdAt: 1000
-  })
-);
-
-store.dispatch(
-  addExpense({
-    description: 'Rent bill',
-    note: 'Rent bill note',
-    amount: 109500
-  })
-);
-
-// const state = store.getState();
-// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-// console.log(visibleExpenses);
 
 const jsx = (
   <Provider store={store}>
